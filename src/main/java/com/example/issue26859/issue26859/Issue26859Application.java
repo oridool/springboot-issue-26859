28fdb363f77aa15d6bf0027b272b247e778e8c48
Ori Doolman <ori.doolman@cyberark.com>
2021-06-20 14:50:12 +0300

    project start

---
+++ src/main/java/com/example/issue26859/issue26859/Issue26859Application.java
@@ -0,0 +1,13 @@
+package com.example.issue26859.issue26859;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issue26859Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Issue26859Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/issue26859/issue26859/Issue26859ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issue26859.issue26859;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issue26859ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
