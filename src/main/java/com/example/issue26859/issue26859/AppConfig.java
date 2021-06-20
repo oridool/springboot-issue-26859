c55b4d8afffeae0d2822056c13e8f7a9eff8319c
Ori Doolman <ori.doolman@cyberark.com>
2021-06-20 15:32:17 +0300

    added ObjectMapper bean default

---
+++ src/main/java/com/example/issue26859/issue26859/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.issue26859.issue26859;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
